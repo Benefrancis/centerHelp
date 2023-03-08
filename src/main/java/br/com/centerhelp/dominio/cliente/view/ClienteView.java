33cdd396808730dda4cfbae1a5f8d88a3dee419d
Benefrancis <benefrancis@gmail.com>
2023-03-08 12:14:06 -0300

    fazendo view e repository

---
+++ src/main/java/br/com/centerhelp/dominio/cliente/view/ClienteView.java
@@ -0,0 +1,28 @@
+package br.com.centerhelp.dominio.cliente.view;
+
+import br.com.centerhelp.dominio.cliente.model.Cliente;
+
+import javax.swing.*;
+import java.time.LocalDate;
+
+public abstract class ClienteView {
+
+    public static Cliente showForm(Cliente c) {
+
+        var tipo = TipoClienteView.select(c == null ? null : c.getTipo());
+        var nome = JOptionPane.showInputDialog("Nome do Cliente", c == null ? "" : c.getNome());
+        var email = JOptionPane.showInputDialog("email do cliente", c == null ? "" : c.getEmail());
+
+
+        var dt = JOptionPane.showInputDialog("Data de Nascimento DD/MM/YYYY");
+        var dia = Integer.parseInt(dt.substring(0, 2));
+        var mes = Integer.parseInt(dt.substring(3, 5));
+        var ano = Integer.parseInt(dt.substring(6, 10));
+
+
+        var nascimento = LocalDate.of(ano, mes, dia);
+
+        return c;
+    }
+
+}
---
+++ src/main/java/br/com/centerhelp/dominio/cliente/view/TipoClienteView.java
@@ -0,0 +1,36 @@
+package br.com.centerhelp.dominio.cliente.view;
+
+import br.com.centerhelp.dominio.cliente.model.TipoCliente;
+import br.com.centerhelp.dominio.cliente.repository.TipoClienteRepository;
+
+import javax.swing.*;
+
+public abstract class TipoClienteView {
+
+    public static TipoCliente showForm(TipoCliente t) {
+        var nome = JOptionPane.showInputDialog("Tipo de Cliente", t == null ? "" : t.getNome());
+        TipoCliente tipo = new TipoCliente();
+        tipo.setNome(nome).setId(t == null ? null : t.getId());
+        return tipo;
+    }
+
+    public static TipoCliente select(TipoCliente t) {
+
+        var tipos = TipoClienteRepository.findAll();
+
+        if (tipos.isEmpty()) return showForm(null);
+
+        TipoCliente ret = (TipoCliente) JOptionPane.showInputDialog(
+                null,
+                "Tipo Cliente",
+                "Tipo Cliente",
+                JOptionPane.QUESTION_MESSAGE,
+                null,
+                tipos.toArray(),
+                t == null ? 1 : t
+        );
+        return ret;
+    }
+
+
+}
