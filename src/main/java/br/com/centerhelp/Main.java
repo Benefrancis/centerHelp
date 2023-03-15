7a5eab3fd12d6949b2f5ba3ab7ccf48d704f2f41
Benefrancis <benefrancis@gmail.com>
2023-03-15 01:07:37 -0300

    Serviço implementado mas ainda não testamos

--- src/main/java/br/com/centerhelp/Main.java
+++ src/main/java/br/com/centerhelp/Main.java
@@ -5,7 +5,6 @@ import br.com.centerhelp.dominio.equipamento.repository.EquipamentoRepository;
 import br.com.centerhelp.dominio.equipamento.repository.TipoEquipamentoRepository;
 import br.com.centerhelp.dominio.equipamento.view.EquipamentoView;
 import br.com.centerhelp.dominio.equipamento.view.TipoEquipamentoView;
-
 import org.glassfish.grizzly.http.server.HttpServer;
 import org.glassfish.jersey.grizzly2.httpserver.GrizzlyHttpServerFactory;
 import org.glassfish.jersey.server.ResourceConfig;
@@ -15,37 +14,22 @@ import java.io.IOException;
 import java.net.URI;
 
 public class Main {
-
-
-    // Base URI the Grizzly HTTP server will listen on
     public static final String BASE_URI = "http://localhost:8080/";
 
-    /**
-     * Starts Grizzly HTTP server exposing JAX-RS resources defined in this application.
-     * @return Grizzly HTTP server.
-     */
     public static HttpServer startServer() {
-        // create a resource config that scans for JAX-RS resources and providers
-        // in br.com.fiap package
         final ResourceConfig rc = new ResourceConfig().packages("br.com.centerhelp.resources");
-
-        // create and start a new instance of grizzly http server
-        // exposing the Jersey application at BASE_URI
         return GrizzlyHttpServerFactory.createHttpServer(URI.create(BASE_URI), rc);
     }
 
-
     public static void main(String[] args) throws IOException {
         final HttpServer server = startServer();
-        System.out.println(String.format("Jersey app started with endpoints available at %s%nHit Ctrl-C to stop it...", BASE_URI));
+        System.out.println(String.format("Jersey app started with endpoints available at " +
+                " %s%nHit Ctrl-C to stop it...", BASE_URI));
         System.in.read();
         server.stop();
     }
-
     public static void viewJoptionPane() {
-
         int opcao = montaMenu();
-
         while (opcao != 0) {
             switch (opcao) {
                 case 1 -> novoEquipamento();
